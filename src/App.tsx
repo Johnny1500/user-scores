82feea52dc72511d549af345bb7c5426cc070fca
Sergey Kiskin <sector1200@gmail.com>
2023-05-16 13:09:42 +0500

    two lists pre-markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+
+interface User {
+  id: string;
+  username: string;
+  avatar: string;
+  score: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(
+        "https://random-data-api.com/api/users/random_user?size=3"
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+
+        const remoteUsers = json.map((user: any) => {
+          const { username, avatar, id } = user;
+
+          return {
+            id,
+            username,
+            avatar,
+            score: 0,
+          };
+        });
+
+        setUsers(remoteUsers);
+        console.log("users", users);
+      } else {
+        throw new Error(response.statusText)
+      }
+    }
+
+    fetchUsers();
+
+    return () => {
+      setUsers([]);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <List ml={10}>
+        {users.map((user) => {
+          const { avatar, username, score, id } = user;
+
+          return <ListItem key={id}>{username}</ListItem>;
+        })}
+      </List>
+      <Tabs size="md" variant="enclosed">
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <List w={250}>
+              <ListItem>Lorem ipsum dolor sit amet</ListItem>
+              <ListItem>Consectetur adipiscing elit</ListItem>
+            </List>
+          </TabPanel>
+          <TabPanel>
+            <List w={250}>
+              <ListItem>Integer molestie lorem at massa</ListItem>
+              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+            </List>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
